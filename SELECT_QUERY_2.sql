885ddf544d15826029020294ce14e3c32cb8e010
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-09-06 19:15:13 -0400

    Create SELECT_QUERY_2.sql

---
+++ SELECT_QUERY_2.sql
@@ -0,0 +1,5 @@
+/*
+Problem Description: https://www.hackerrank.com/challenges/revising-the-select-query-2?h_r=next-challenge&h_v=zen
+*/
+
+SELECT NAME FROM CITY WHERE POPULATION>120000 AND COUNTRYCODE='USA'
