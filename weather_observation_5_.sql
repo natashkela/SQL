cd200cac0449b28b62a8c44fe3ee4e2a8d9567f0
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-09-06 19:38:16 -0400

    Create weather_observation_5_.sql

---
+++ weather_observation_5_.sql
@@ -0,0 +1,6 @@
+/*
+Problem Description]: https://www.hackerrank.com/challenges/weather-observation-station-5
+*/
+
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY), CITY LIMIT 1; 
+SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY DESC LIMIT 1;
