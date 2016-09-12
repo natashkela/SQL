f96de29922e9c6b78eb63abc5843b0869ec80066
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-09-11 21:47:47 -0400

    Create employee_name.sql

---
+++ employee_name.sql
@@ -0,0 +1,10 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+/*
+Problem Description: https://www.hackerrank.com/challenges/name-of-employees
+*/
+
+SELECT NAME FROM EMPLOYEE ORDER BY NAME
