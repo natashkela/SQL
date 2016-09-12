01be23174d08ec5850df24a450a2a05decb5d7de
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-09-11 21:21:05 -0400

    Create weather_observation_7.sql

---
+++ weather_observation_7.sql
@@ -0,0 +1,9 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+/*
+Problem Description: https://www.hackerrank.com/challenges/weather-observation-station-7
+*/
+SELECT DISTINCT CITY FROM STATION WHERE CITY LIKE '%a' OR CITY LIKE '%e' OR CITY LIKE '%i' OR CITY LIKE '%o'OR CITY LIKE '%u'
