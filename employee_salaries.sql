78b7612036c7dd5992ae45276d9100558cf15ce2
Nata Vacheishvili <nvacheishvili@wpi.edu>
2016-09-11 21:50:12 -0400

    Create employee_salaries.sql

---
+++ employee_salaries.sql
@@ -0,0 +1,10 @@
+/*
+Enter your query here.
+Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+*/
+
+/*
+Problem Description: https://www.hackerrank.com/challenges/salary-of-employees?h_r=next-challenge&h_v=zen
+*/
+
+SELECT NAME FROM EMPLOYEE WHERE SALARY>2000 AND MONTHS<10 ORDER BY EMPLOYEE_ID
